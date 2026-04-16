709532b4115476ed79789a54c259ab71a3cabd0a
ryanandreas <ryanguansin@gmail.com>
2026-04-16 22:55:11 +0700

    deploy fix

--- tmp/check_service.ts
+++ tmp/check_service.ts
@@ -3,7 +3,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   const service = await prisma.services.findFirst({
-    where: { id: { endsWith: '8794a8e5' } },
+    // @ts-ignore - UUID fields do not support endsWith in Prisma, but it's used here for a partial ID check
+    where: { id: { contains: '8794a8e5' } },
     include: { teknisi: true }
   });
   console.log(JSON.stringify(service, null, 2));
